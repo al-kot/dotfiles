e7284c87d19e0ed3ff857e7a77f49bafde4b7444
asq <yacar0009@gmail.com>
2024-01-21 22:23:56 +0100

    colors

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -8,8 +8,8 @@ killall -q polybar
 
 # Launch bar1 and bar2
 # echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar top 2>&1 | tee -a /tmp/polybar1.log &
-disown
+# polybar top 2>&1 | tee -a /tmp/polybar1.log &
+# disown
 polybar bottom 2>&1 | tee -a /tmp/polybar2.log &
 disown
 
