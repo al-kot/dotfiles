ce00600a87217b418a316c5372ec5e67041a35bf
Aleksei Kotliarov <aleksei.kotliarov@epita.fr>
2024-01-23 18:42:27 +0100

    nix

--- neovim/.config/nvim/lua/plugins/others.lua
+++ neovim/.config/nvim/lua/plugins/others.lua
@@ -6,4 +6,30 @@ return {
         },
         lazy = false,
     },
+    {
+        "norcalli/nvim-colorizer.lua",
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        opts = function(_, opts)
+            opts.transparent = true
+            opts.colors = {
+                theme = {
+                    all = {
+                        ui = {
+                            bg_gutter = "none",
+                            float = {
+                                bg = "none",
+                            },
+                        },
+                    },
+                },
+            }
+        end,
+    },
+    {
+        "christoomey/vim-tmux-navigator",
+        lazy = false,
+        enabled = true,
+    },
 }
