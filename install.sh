4a6d8d6741c136c51710715fbf511c3e147fc552
Aleksei Kotliarov <aleksei.kotliarov@epita.fr>
2025-02-13 12:04:30 +0100

    hz

--- install.sh
+++ install.sh
@@ -10,11 +10,18 @@ install() {
 
 {
 
-install stow wezterm zsh nerd-fonts.mononoki starship tmux polybar lsd rufi
+install stow wezterm zsh starship tmux polybar lsd rufi
+
+nix profile install --impure --expr 'with (builtins.getFlake "nixpkgs").legacyPackages.${builtins.currentSystem};
+    nerdfonts.override {
+      fonts = [
+        "Mononoki"
+    ];
+  }'
 
 rm -rf "$HOME/.tmux" "$HOME/.tmux.conf" "$HOME/.zsh" "$HOME/.zshrc"
 
-stow -t "$HOME" wezterm zsh tmux polybar picom lsd rufi i3 neovim
+stow wezterm zsh tmux polybar picom lsd rufi i3 neovim
 
 git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME"/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME"/.zsh/syntax-highlighting 
