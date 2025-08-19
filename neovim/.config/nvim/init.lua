04af5dbdd736ca62d5c4e0bd8b4716d8d37be234
Aleksei <aleksei.kotliarov@epita.fr>
2025-08-19 15:21:16 +0200

    typst-preview

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -49,3 +49,26 @@ require('plugins')
 require('lsp')
 require('statusline')
 require('autocmds')
+
+vim.pack.add({
+    'https://github.com/aleshasuqa/typst-preview.nvim.git',
+})
+
+local typst = require('typst-preview')
+typst.setup({
+    preview = {
+        position = 'right',
+        ppi = 120,
+    }
+})
+utils.add_keybinds({
+    { 'n', '<leader>tn',  function() typst.next_page() end },
+    { 'n', '<leader>te',  function() typst.prev_page() end },
+    { 'n', '<leader>tgg', function() typst.first_page() end },
+    { 'n', '<leader>tG',  function() typst.last_page() end },
+    -- { 'n', '<leader>tr',  compile_and_render },
+    -- { 'n', '<leader>tc',  clear_preview },
+    { 'n', '<leader>td', function() typst.stop() end },
+    { 'n', '<leader>to', function() typst.start() end },
+    { 'n', '<leader>tr', function() typst.refresh() end },
+})
